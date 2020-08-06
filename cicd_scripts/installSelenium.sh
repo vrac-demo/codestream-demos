336d11196f16d942aae3af0ec2b9586d93ccac8f
Jeremy Lemmon <jlemmon@jlemmon-a02.vmware.com>
2020-08-06 16:43:29 -0500

    stderr too

--- cicd_scripts/installSelenium.sh
+++ cicd_scripts/installSelenium.sh
@@ -4,6 +4,6 @@ cd /tmp/
 wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
-curl https://intoli.com/install-google-chrome.sh | bash >> /tmp/chromeinstall.txt
+curl https://intoli.com/install-google-chrome.sh | bash &> /tmp/chromeinstall.txt
 sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
 google-chrome --version && which google-chrome
