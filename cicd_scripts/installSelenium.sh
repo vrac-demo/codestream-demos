61be99c5752209a3a262ad92e047112dd85dd90a
Jeremy Lemmon <jlemmon@jlemmon-a02.vmware.com>
2020-08-06 15:07:41 -0500

    adding basic tests

---
+++ cicd_scripts/installSelenium.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip install selenium --user
+cd /tmp/
+wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+curl https://intoli.com/install-google-chrome.sh | bash
+sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+google-chrome --version && which google-chrome
---
+++ cicd_scripts/seleniumTestTitle.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+print("Starting Selenium test")
+
+options = Options()
+options.headless = True
+
+driver = webdriver.Chrome(options=options)
+driver.get("__URL_TO_TEST__")
+
+time.sleep(5) # wait for page load
+
+print(driver.getTitle())
+
+element_text = driver.find_element_by_id("title").text
+
+print(element_text)
