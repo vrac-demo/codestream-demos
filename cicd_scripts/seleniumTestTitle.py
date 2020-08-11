415c43222bdc6b2e50c9b8655f370974c3c1ebd7
Jeremy Lemmon <jlemmon@jlemmon-a02.vmware.com>
2020-08-11 14:40:19 -0500

    changing path

--- cicd_scripts/seleniumTestTitle.py
+++ cicd_scripts/seleniumTestTitle.py
@@ -4,7 +4,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 
-driver = webdriver.Chrome('/usr/lib/chromedriver', chrome_options=chrome_options,  service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options,  service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
 
 
 driver.get('https://google.org')
