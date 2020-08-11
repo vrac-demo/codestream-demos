0e488122abca1c09632cdc049ef82a0217481a17
Jeremy Lemmon <jlemmon@jlemmon-a02.vmware.com>
2020-08-11 14:23:26 -0500

    disable gpu

--- cicd_scripts/seleniumTestTitle.py
+++ cicd_scripts/seleniumTestTitle.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome('/usr/lib/chromedriver', chrome_options=chrome_options,  service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
 
