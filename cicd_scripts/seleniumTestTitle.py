812d306d0f4e64ac4ae665bcd58587e15d1a83d8
Jeremy Lemmon <jlemmon@jlemmon-a02.vmware.com>
2020-08-11 13:32:30 -0500

    simplifying test

--- cicd_scripts/seleniumTestTitle.py
+++ cicd_scripts/seleniumTestTitle.py
@@ -1,19 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-print("Starting Selenium test")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
 
-options = Options()
-options.headless = True
+driver = webdriver.Chrome('/usr/lib/chromedriver', chrome_options=chrome_options,  service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
 
-driver = webdriver.Chrome(options=options)
-#driver.get("__URL_TO_TEST__")
-driver.get("https://www.youtube.com")
 
-time.sleep(5) # wait for page load
-
-print(driver.getTitle())
-
-element_text = driver.find_element_by_id("title").text
-
-print(element_text)
+driver.get('https://google.org')
+print(driver.title)
